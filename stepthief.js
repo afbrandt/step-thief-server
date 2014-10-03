88ebfd687bc9b0529645ca2b709aa2d99a0f714d
Andrew Brandt <Andrew@Xerxes.local>
2014-10-03 13:59:24 -0700

    registration endpoint available via POST method

--- stepthief.js
+++ stepthief.js
@@ -9,7 +9,16 @@ app.use(bodyParser())
 var db = mongoskin.db('mongodb://xerxes-dev:dorystudios@ds043170.mongolab.com:43170/stepthief-dev')
 
 app.get('/v1/', function(req, res) {
+  console.log('received a GET to /v1/')
   res.send('service is up, try /v1/users/')
+})
+
+app.post('/v1/register', function(req, res) {
+  console.log('received POST to /v1/register')
+  db.collection('v1-users').insert(req.body, {}, function(e, results){
+    if (e) return next(e)
+    res.send(results)
+  })
 })
 
 app.listen(3000)
